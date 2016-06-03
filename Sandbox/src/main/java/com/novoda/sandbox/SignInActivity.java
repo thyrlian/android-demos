47f5b81640e5d3817b73102e520c8bff85f62fba
Jing Li <thyrlian@gmail.com>
2016-06-03 23:36:33 +0200

    Trim space off on text change of username field

--- Sandbox/src/main/java/com/novoda/sandbox/SignInActivity.java
+++ Sandbox/src/main/java/com/novoda/sandbox/SignInActivity.java
@@ -35,8 +35,8 @@ public class SignInActivity extends Activity {
         passwordEditText = (EditText) findViewById(R.id.sign_in_activity_password_field);
         submitButton = findViewById(R.id.sign_in_activity_submit_button);
 
-        usernameEditText.addTextChangedListener(signInEnablingSubmitTextWatcher);
-        passwordEditText.addTextChangedListener(signInEnablingSubmitTextWatcher);
+        usernameEditText.addTextChangedListener(new SignInTextWatcher(usernameEditText, false));
+        passwordEditText.addTextChangedListener(new SignInTextWatcher(passwordEditText, true));
         submitButton.setEnabled(false);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +83,15 @@ public class SignInActivity extends Activity {
         }
     }
 
-    private TextWatcher signInEnablingSubmitTextWatcher = new TextWatcher() {
+    private class SignInTextWatcher implements TextWatcher {
+        private EditText editText;
+        private boolean isSpaceAllowed;
+
+        public SignInTextWatcher(EditText editText, boolean isSpaceAllowed) {
+            this.editText = editText;
+            this.isSpaceAllowed = isSpaceAllowed;
+        }
+
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             // do nothing
@@ -96,7 +104,14 @@ public class SignInActivity extends Activity {
 
         @Override
         public void afterTextChanged(Editable s) {
-            // do nothing
+            if (!isSpaceAllowed) {
+                String noSpaceText = s.toString().replaceAll(" ", "");
+                if (!noSpaceText.equals(s.toString())) {
+                    int selectionIndex = editText.getSelectionStart() - 1;
+                    editText.setText(noSpaceText);
+                    editText.setSelection(selectionIndex);
+                }
+            }
         }
-    };
+    }
 }
