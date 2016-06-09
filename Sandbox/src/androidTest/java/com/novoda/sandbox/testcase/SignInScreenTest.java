a7be693824788ca3d31050ecc191d691fe4dfeac
Jing Li <thyrlian@gmail.com>
2016-06-10 01:25:22 +0200

    Add sign in ui test

--- Sandbox/src/androidTest/java/com/novoda/sandbox/testcase/SignInScreenTest.java
+++ Sandbox/src/androidTest/java/com/novoda/sandbox/testcase/SignInScreenTest.java
@@ -1,7 +1,32 @@
 package com.novoda.sandbox.testcase;
 
+import android.support.test.rule.ActivityTestRule;
+
+import com.novoda.sandbox.feature.signin.SignInActivity;
+import com.novoda.sandbox.screen.SignInScreen;
+import com.novoda.sandbox.util.TestRuleHelper;
+
+import org.junit.Rule;
+import org.junit.Test;
+
 /**
  * Created by jingli on 06/06/16.
  */
 public class SignInScreenTest {
+
+    @Rule
+    public ActivityTestRule<SignInActivity> signInActivityTestRule = TestRuleHelper.createTestRuleWithLoginState(SignInActivity.class, true);
+
+    private SignInScreen signInScreen = new SignInScreen();
+
+    /**
+     * Given I try and click submit
+     * When I have not entered a username or password
+     * Then the submit button is not clickable
+     */
+    @Test
+    public void checkSubmitButtonIsNotEnabledWithoutCredential() {
+        signInScreen.verifySubmitButtonIsNotEnabled();
+    }
+
 }
